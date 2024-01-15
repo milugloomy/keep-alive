f821e44efca13605d9b55ead7d6b9d01243ab06b
qijunhao <qijunhao@bytedance.com>
2024-01-15 11:31:50 +0800

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,23 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
-
-const path = require('path');
-
-const isProduction = process.env.NODE_ENV == 'production';
+const isProduction = process.env.NODE_ENV === 'production';
 
 
 const config = {
   entry: './src/index.ts',
-  output: {
-    path: path.resolve(__dirname, 'dist')
-  },
   plugins: [
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
   ],
+  experiments: {
+    outputModule: true
+  },
+  output: {
+    filename: '[name].js',
+    library: {
+      type: 'module'
+    }
+  },
+  externals: {
+    react: 'react'
+  },
   module: {
     rules: [
       {
@@ -25,17 +29,24 @@ const config = {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
         type: 'asset'
       }
-
-      // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
     ]
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.jsx', '.js', '...']
+  },
+  optimization: {
+    minimize: false,
+    // 拆包不生效
+    // splitChunks: {
+    //   chunks: 'all',
+    //   minSize: 1,
+    //   minSizeReduction: 0
+    // }
   }
+
 };
 
-module.exports = () => {
+export default () => {
   if (isProduction) {
     config.mode = 'production';
 
